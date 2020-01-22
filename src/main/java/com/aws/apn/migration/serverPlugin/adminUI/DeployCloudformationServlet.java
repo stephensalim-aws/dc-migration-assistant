ca7e8f2523fbd8f2e6bd1aa69334f3c4cffdd2e5
Andy Powell <andpow@amazon.de>
2020-01-22 11:05:29 +0100

    Completed Network parameters Completed accessKey / secretKey / region configuration Made AZ selection dependent on region selection

--- src/main/java/com/aws/apn/migration/serverPlugin/adminUI/DeployCloudformationServlet.java
+++ src/main/java/com/aws/apn/migration/serverPlugin/adminUI/DeployCloudformationServlet.java
@@ -1,8 +1,18 @@
 package com.aws.apn.migration.serverPlugin.adminUI;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.ec2.AmazonEC2;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.auth.LoginUriProvider;
+import com.atlassian.sal.api.pluginsettings.PluginSettings;
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import com.atlassian.sal.api.user.UserManager;
 import com.atlassian.templaterenderer.TemplateRenderer;
 
@@ -13,23 +23,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @Scanned
 public class DeployCloudformationServlet extends HttpServlet {
-
+    private static final String PLUGIN_STORAGE_KEY = "com.aws.apn.migration.serverPlugin";
     @ComponentImport
     private final UserManager userManager;
     @ComponentImport
     private final LoginUriProvider loginUriProvider;
     @ComponentImport
     private final TemplateRenderer renderer;
+    @ComponentImport
+    private final PluginSettingsFactory pluginSettingsFactory;
 
     @Inject
-    public DeployCloudformationServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer) {
+    public DeployCloudformationServlet(UserManager userManager, LoginUriProvider loginUriProvider, TemplateRenderer renderer, PluginSettingsFactory pluginSettingsFactory) {
         this.userManager = userManager;
         this.loginUriProvider = loginUriProvider;
         this.renderer = renderer;
+        this.pluginSettingsFactory = pluginSettingsFactory;
     }
 
     @Override
@@ -40,8 +58,17 @@ public class DeployCloudformationServlet extends HttpServlet {
             return;
         }
 
+        Map<String, Object> context = new HashMap<String, Object>();
+        PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
+
+        String region = (String) pluginSettings.get(PLUGIN_STORAGE_KEY + ".awsRegion");
+        String secretKey = (String) pluginSettings.get(PLUGIN_STORAGE_KEY + ".secretKey");
+        String accessKey = (String) pluginSettings.get(PLUGIN_STORAGE_KEY + ".accessKey");
+
+        List<String> availabilityZones = this.getAvailabilityZones(region, accessKey, secretKey);
+        context.put("availabilityZones", availabilityZones);
         response.setContentType("text/html;charset=utf-8");
-        renderer.render("cloudformation.vm", response.getWriter());
+        renderer.render("cloudformation.vm", context, response.getWriter());
     }
 
     private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -55,5 +82,16 @@ public class DeployCloudformationServlet extends HttpServlet {
             builder.append(request.getQueryString());
         }
         return URI.create(builder.toString());
+    }
+
+    private List<String> getAvailabilityZones(final String region, final String accessKey, final String secretKey) {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+        AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withCredentials(credentialsProvider).withRegion(region).build();
+        DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
+        try (Stream<AvailabilityZone> zoneStream = zones_response.getAvailabilityZones().stream()) {
+            return zoneStream.map(AvailabilityZone::getZoneName).sorted().collect(Collectors.toList());
+        }
+
     }
 }
