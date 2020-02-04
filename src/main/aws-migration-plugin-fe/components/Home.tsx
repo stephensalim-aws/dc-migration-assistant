bff436755075838146c3e612a0d5c5a2654fe26f
Ben Partridge <bpartridge@atlassian.com>
2020-02-04 17:08:33 +1100

    MOT-57: Resolve linting errors in RoutePaths.ts

--- src/main/aws-migration-plugin-fe/components/Home.tsx
+++ src/main/aws-migration-plugin-fe/components/Home.tsx
@@ -3,7 +3,7 @@ import Button from '@atlaskit/button';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import { overviewPath } from '../utils/RoutePaths';
+import { overviewPath } from '../utils/RoutePaths.ts';
 
 type HomeProps = {
     title: string;
--- src/main/aws-migration-plugin-fe/utils/RoutePaths.ts
+++ src/main/aws-migration-plugin-fe/utils/RoutePaths.ts
@@ -1,6 +1,7 @@
+// eslint-disable-next-line import/no-unresolved
 import contextPath from 'wrm/context-path';
 
-const routePrefix = (() => {
+const routePrefix = ((): string => {
     const awsMigrationServletPath = '/plugins/servlet/aws-migration-plugin';
     const pathname = contextPath(); // eslint-disable-line no-undef
 
