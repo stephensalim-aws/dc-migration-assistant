6f50a66c212e40084b4912ed7b2b6c0b60815aa3
Ben Partridge <bpartridge@atlassian.com>
2020-02-10 16:59:48 +1100

    MOT-74: Implement active objects tests

--- src/test/java/com/aws/apn/migration/awsmigrationplugin/core/AWSMigrationServiceTest.java
+++ src/test/java/com/aws/apn/migration/awsmigrationplugin/core/AWSMigrationServiceTest.java
@@ -1,34 +1,88 @@
 package com.aws.apn.migration.awsmigrationplugin.core;
 
+import com.atlassian.activeobjects.external.ActiveObjects;
+import com.atlassian.activeobjects.test.TestActiveObjects;
+import com.aws.apn.migration.awsmigrationplugin.dto.Migration;
 import com.aws.apn.migration.awsmigrationplugin.spi.MigrationStage;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import net.java.ao.EntityManager;
+import net.java.ao.test.junit.ActiveObjectsJUnitRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static com.aws.apn.migration.awsmigrationplugin.spi.MigrationStage.STARTED;
 import static com.aws.apn.migration.awsmigrationplugin.spi.MigrationStage.UNSTARTED;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+// We have to use the JUnit 4 API because there is no JUnit 5 active objects extension :(
+@RunWith(ActiveObjectsJUnitRunner.class)
 public class AWSMigrationServiceTest {
+
+    private ActiveObjects ao;
+
+    private EntityManager entityManager;
 
     private AWSMigrationService sut;
 
-    @BeforeEach
-    public void initialiseSystemUnderTest() {
-        this.sut = new AWSMigrationService();
+    @Before
+    public void setup() {
+        assertNotNull(entityManager);
+        ao = new TestActiveObjects(entityManager);
+        sut = new AWSMigrationService(ao);
     }
 
     @Test
     public void testStageWhenNoMigrationCreatedIsUnstarted() {
+        ao.migrate(Migration.class);
+
         MigrationStage initialStage = sut.getMigrationStage();
 
         assertEquals(UNSTARTED, initialStage);
     }
 
     @Test
-    public void testStageIsStartedAfterStartingMigration() {
-        sut.startMigration();
+    public void testStageOfCurrentMigrationIsReturned() {
+        ao.migrate(Migration.class);
+
+        Migration migration = ao.create(Migration.class);
+        migration.setStage(STARTED);
+        migration.save();
+
+        assertEquals(1, ao.find(Migration.class).length);
+
+        ao.flushAll();
+
         MigrationStage currentStage = sut.getMigrationStage();
 
         assertEquals(STARTED, currentStage);
+    }
+
+    @Test
+    public void testStageIsStartedAfterStartingMigration() {
+        ao.migrate();
+        assertEquals(0, ao.find(Migration.class).length);
+
+        assertTrue(sut.startMigration());
+        MigrationStage currentStage = sut.getMigrationStage();
+
+        assertEquals(STARTED, currentStage);
+        assertEquals(1, ao.find(Migration.class).length);
+    }
+
+    @Test
+    public void testStageIsUnstartedWhenNoMigrationExists() {
+        ao.migrate(Migration.class);
+        Migration migration = ao.create(Migration.class);
+        migration.setStage(STARTED);
+        migration.save();
+
+        ao.flushAll();
+        assertEquals(1, ao.find(Migration.class).length);
+
+        assertFalse(sut.startMigration());
     }
 }
