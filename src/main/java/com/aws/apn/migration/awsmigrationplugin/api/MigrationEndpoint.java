b65b11c0143684ea543527745f18915b482329a3
Ben Partridge <bpartridge@atlassian.com>
2020-02-10 14:23:00 +1100

    MOT-74: Implement getMigration API endpoint

--- src/main/java/com/aws/apn/migration/awsmigrationplugin/api/MigrationEndpoint.java
+++ src/main/java/com/aws/apn/migration/awsmigrationplugin/api/MigrationEndpoint.java
@@ -1,5 +1,8 @@
 package com.aws.apn.migration.awsmigrationplugin.api;
 
+import com.aws.apn.migration.awsmigrationplugin.spi.MigrationService;
+import com.aws.apn.migration.awsmigrationplugin.spi.MigrationStage;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -11,17 +14,33 @@ import javax.ws.rs.core.Response;
 @Path("/migration")
 public class MigrationEndpoint {
 
+    private MigrationService migrationService;
+
+    public MigrationEndpoint(MigrationService migrationService) {
+        this.migrationService = migrationService;
+    }
+
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response getMigration() {
-        return Response.ok().build();
+        if (migrationService.getMigrationStage() == MigrationStage.UNSTARTED) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .build();
+        } else {
+            return Response
+                    .ok(migrationService.getMigrationStage().toString())
+                    .build();
+        }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response createMigration() {
-        return Response.ok().build();
+        return Response.
+                ok()
+                .build();
     }
 }
--- src/main/java/com/aws/apn/migration/awsmigrationplugin/spi/MigrationStage.java
+++ src/main/java/com/aws/apn/migration/awsmigrationplugin/spi/MigrationStage.java
@@ -1,5 +1,16 @@
 package com.aws.apn.migration.awsmigrationplugin.spi;
 
 public enum MigrationStage {
-    UNSTARTED, STARTED
+    UNSTARTED(""), STARTED("started");
+
+    private String key;
+
+    MigrationStage(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return key;
+    }
 }
