20827751c009284eb4dee29dd22cfa35b746978f
Ben Partridge <bpartridge@atlassian.com>
2020-02-10 12:15:16 +1100

    MOT-74: Add test package structure and placeholder for AWSMigrationServiceTest

--- src/test/java/com/aws/apn/migration/awsmigrationplugin/core/AWSMigrationServiceTest.java
+++ src/test/java/com/aws/apn/migration/awsmigrationplugin/core/AWSMigrationServiceTest.java
@@ -1,4 +1,13 @@
 package com.aws.apn.migration.awsmigrationplugin.core;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class AWSMigrationServiceTest {
+
+    @Test
+    public void testStageWhenNoMigrationCreatedIsUnstarted() {
+        fail();
+    }
 }
