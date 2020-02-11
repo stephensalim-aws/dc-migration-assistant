92b0d0fe9d57219e241cc7c4c1930a4650faac60
Ben Partridge <bpartridge@atlassian.com>
2020-02-11 14:30:59 +1100

    MOT-77: Implement rendering of QS parameters with "AllowedValues"

--- src/main/aws-migration-plugin-fe/components/QuickStartForm.tsx
+++ src/main/aws-migration-plugin-fe/components/QuickStartForm.tsx
@@ -1,15 +1,16 @@
-import React, { FunctionComponent, ReactElement, ReactNode } from 'react';
+import React, { FunctionComponent, ReactElement, ReactNode, useState, useEffect } from 'react';
 import yaml from 'yaml';
-import { string } from 'prop-types';
+import Select from '@atlaskit/select';
+import Toggle from '@atlaskit/toggle';
 
 const quickstartUrl =
     'https://dcd-slinghost-templates.s3.amazonaws.com/quickstart-jira-dc-with-vpc.template.parameters.yaml';
 
 type QuickStartParameterYamlNode = {
     Type: string;
-    Default: string | number;
+    Default: string | number | boolean;
     Description: string;
-    AllowedValues?: Array<string>;
+    AllowedValues?: Array<string | boolean>;
     ConstraintDescription?: string;
     AllowedPattern?: string;
     MaxLength?: number;
@@ -31,25 +32,34 @@ type QuickStartParameterYamlNode = {
 const createSelectFromQuickstartParam = (
     key: string,
     param: QuickStartParameterYamlNode
-): ReactNode => {
-    return <div />;
+): ReactElement => {
+    if (param.AllowedValues.length === 2 && typeof param.AllowedValues[0] === 'boolean') {
+        return <Toggle key={key} size="large" isDefaultChecked={param.Default as boolean} />;
+    }
+    return (
+        <Select
+            key={key}
+            options={param.AllowedValues.map(val => ({ label: val, value: val }))}
+            value={{ label: param.Default, value: param.Default }}
+        />
+    );
 };
 
-const createAZSelection = (key: string, param: QuickStartParameterYamlNode): ReactNode => {
-    return <div />;
+const createAZSelection = (key: string, param: QuickStartParameterYamlNode): ReactElement => {
+    return <div key={key} />;
 };
 
 const createInputFromQuickstartParam = (
     key: string,
     param: QuickStartParameterYamlNode
-): ReactNode => {
-    return <div />;
+): ReactElement => {
+    return <div key={key} />;
 };
 
 const quickstartParamToAtlaskitFormElement = (
     key: string,
     param: QuickStartParameterYamlNode
-): ReactNode => {
+): ReactElement => {
     if (param.AllowedValues) {
         return createSelectFromQuickstartParam(key, param);
     }
@@ -60,21 +70,32 @@ const quickstartParamToAtlaskitFormElement = (
 };
 
 export const QuickstartForm: FunctionComponent = (): ReactElement => {
-    fetch(quickstartUrl, {
-        method: 'GET',
-    })
-        .then(resp => resp.text())
-        .then(text => {
-            const paramDoc = yaml.parse(text);
-            const params = paramDoc.Parameters;
-            console.log(params);
+    const [params, setParams] = useState({});
+    const [hasUpdatedTemplate, setHasUpdatedTemplate] = useState(false);
 
-            Object.entries(params).forEach((entry: [string, QuickStartParameterYamlNode]) => {
+    useEffect(() => {
+        if (!hasUpdatedTemplate) {
+            fetch(quickstartUrl, {
+                method: 'GET',
+            })
+                .then(resp => resp.text())
+                .then(text => {
+                    const paramDoc = yaml.parse(text);
+                    console.log(paramDoc);
+                    setParams(paramDoc.Parameters);
+                    setHasUpdatedTemplate(true);
+                });
+        }
+    });
+
+    // console.log(params);
+
+    return (
+        <div>
+            {Object.entries(params).map((entry: [string, QuickStartParameterYamlNode]) => {
                 const [key, value] = entry;
-                console.log(key);
-                console.log(value);
-            });
-        });
-
-    return <h1>Hello</h1>;
+                return quickstartParamToAtlaskitFormElement(key, value);
+            })}
+        </div>
+    );
 };
