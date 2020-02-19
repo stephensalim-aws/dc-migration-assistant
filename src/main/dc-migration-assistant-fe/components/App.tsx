d91d4bd2dcdfbc6e3692db2ead347dbc5c1ede82
Ben Partridge <bpartridge@atlassian.com>
2020-02-19 12:04:56 +1100

    CHET-92: Extract AWS routes into separate component

--- src/main/dc-migration-assistant-fe/components/App.tsx
+++ src/main/dc-migration-assistant-fe/components/App.tsx
@@ -2,54 +2,27 @@ import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { I18n } from '@atlassian/wrm-react-i18n';
 
-import { awsAuthPath, overviewPath, quickstartPath } from '../utils/RoutePaths';
+import { overviewPath } from '../utils/RoutePaths';
 import { MigrationOverview } from './MigrationOverview';
-import { QuickStartDeploy } from './aws/quickstart/QuickStartDeploy';
-import { AuthenticateAWS } from './aws/auth/AuthenticateAWS';
 import { Home } from './Home';
+import { AWSRoutes } from './aws/AwsRoutes';
 
-export const App: FunctionComponent = () => {
-    const regions = [
-        'us-east-1',
-        'us-east-2',
-        'us-west-1',
-        'us-west-2',
-        'we really need to populate this via API',
-    ];
-
-    return (
-        <Router>
-            <Switch>
-                <Route path={overviewPath}>
-                    <MigrationOverview />
-                </Route>
-                <Route path={quickstartPath}>
-                    <QuickStartDeploy />
-                </Route>
-                <Route path={awsAuthPath}>
-                    <AuthenticateAWS
-                        getRegions={(): Promise<Array<string>> => {
-                            // This should be replaced with API call which gets regions
-                            return Promise.resolve(regions);
-                        }}
-                        onSubmitCreds={(creds): Promise<string> => {
-                            // This should be replaced with API call that stores the credentials
-                            // eslint-disable-next-line no-console
-                            console.log(creds);
-                            return Promise.resolve('');
-                        }}
-                    />
-                </Route>
-                <Route>
-                    <Home
-                        title={I18n.getText('atlassian.migration.datacenter.home.title')}
-                        synopsis={I18n.getText('atlassian.migration.datacenter.home.synopsis')}
-                        exploreMigrationButtonText={I18n.getText(
-                            'atlassian.migration.datacenter.home.explore.migration'
-                        )}
-                    />
-                </Route>
-            </Switch>
-        </Router>
-    );
-};
+export const App: FunctionComponent = () => (
+    <Router>
+        <Switch>
+            <AWSRoutes />
+            <Route path={overviewPath}>
+                <MigrationOverview />
+            </Route>
+            <Route>
+                <Home
+                    title={I18n.getText('atlassian.migration.datacenter.home.title')}
+                    synopsis={I18n.getText('atlassian.migration.datacenter.home.synopsis')}
+                    exploreMigrationButtonText={I18n.getText(
+                        'atlassian.migration.datacenter.home.explore.migration'
+                    )}
+                />
+            </Route>
+        </Switch>
+    </Router>
+);
