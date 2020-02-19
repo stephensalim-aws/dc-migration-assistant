1ed6bb0ca75a50b0e8ad69e99be3bf39b8722fef
Ben Partridge <bpartridge@atlassian.com>
2020-02-19 13:30:59 +1100

    CHET-107: Create implementation for aws sdk v1 and v2 credentials providers which defers to default provider

---
+++ src/main/java/com/atlassian/migration/datacenter/core/aws/AtlassianPluginAWSCredentialsProvider.java
@@ -0,0 +1,38 @@
+package com.atlassian.migration.datacenter.core.aws;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+
+public class AtlassianPluginAWSCredentialsProvider implements AwsCredentialsProvider, AWSCredentialsProvider {
+
+    /**
+     * AWS SDK V1 credentials API
+     * @return valid AWS credentials to be used to create AWS API clients
+     */
+    @Override
+    public AWSCredentials getCredentials() {
+        return new DefaultAWSCredentialsProviderChain().getCredentials();
+    }
+
+    /**
+     * AWS SDK V1 credentials API
+     * Refreshes the AWS credentials provided by this provider
+     */
+    @Override
+    public void refresh() {
+        new DefaultAWSCredentialsProviderChain().refresh();
+    }
+
+    /**
+     * AWS SDK V2 credentials API
+     * @return
+     */
+    @Override
+    public AwsCredentials resolveCredentials() {
+        return DefaultCredentialsProvider.create().resolveCredentials();
+    }
+}
