a36c4c78719e91772e0dbf0d6051b589abb89ed1
Ben Partridge <bpartridge@atlassian.com>
2020-02-20 16:59:05 +1100

    CHET-107: Default to default region if no region has been set

--- src/main/java/com/atlassian/migration/datacenter/core/aws/region/PluginSettingsRegionManager.java
+++ src/main/java/com/atlassian/migration/datacenter/core/aws/region/PluginSettingsRegionManager.java
@@ -1,5 +1,6 @@
 package com.atlassian.migration.datacenter.core.aws.region;
 
+import com.amazonaws.regions.Regions;
 import com.atlassian.migration.datacenter.core.aws.GlobalInfrastructure;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.atlassian.sal.api.pluginsettings.PluginSettings;
@@ -7,6 +8,10 @@ import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * Manages the persistence and retrieval of the region used to make AWS SDK API calls.
+ * The region is stored in the plugin settings of this app.
+ */
 @Component
 public class PluginSettingsRegionManager implements RegionStorer, RegionFetcher {
 
@@ -22,12 +27,26 @@ public class PluginSettingsRegionManager implements RegionStorer, RegionFetcher 
         this.globalInfrastructure = globalInfrastructure;
     }
 
+    /**
+     * @return The id of the region that has been stored most recently (e.g. us-east-2, ap-southeast-1). If no region
+     * has been configured, it will return the id of the default region.
+     */
     @Override
     public String getRegion() {
         PluginSettings pluginSettings = pluginSettingsFactory.createGlobalSettings();
-        return (String) pluginSettings.get(AWS_REGION_PLUGIN_STORAGE_KEY + REGION_PLUGIN_STORAGE_SUFFIX);
+        String pluginSettingsRegion = (String) pluginSettings.get(AWS_REGION_PLUGIN_STORAGE_KEY + REGION_PLUGIN_STORAGE_SUFFIX);
+        if (pluginSettingsRegion == null || pluginSettingsRegion == "") {
+            return Regions.DEFAULT_REGION.getName();
+        }
+        return pluginSettingsRegion;
     }
 
+    /**
+     * Sets the region to be used for AWS API calls
+     * @param region the id of the region to use e.g. us-east-1, eu-central-1
+     * @throws InvalidAWSRegionException if the region id provided is not a supported AWS region.
+     * @see GlobalInfrastructure
+     */
     @Override
     public void storeRegion(String region) throws InvalidAWSRegionException {
         if(!isValidRegion(region)) {
