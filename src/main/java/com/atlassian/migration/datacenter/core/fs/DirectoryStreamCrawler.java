066756148966665103e7ea66cd081631dc2ff084
Adam Brokes <abrokes@atlassian.com>
2020-02-24 14:03:28 +1100

    CHET-93: Add FailableOperation interface to surface non-fatal errors

--- src/main/java/com/atlassian/migration/datacenter/core/fs/DirectoryStreamCrawler.java
+++ src/main/java/com/atlassian/migration/datacenter/core/fs/DirectoryStreamCrawler.java
@@ -1,12 +1,20 @@
 package com.atlassian.migration.datacenter.core.fs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class DirectoryStreamCrawler implements Crawler {
+    private static final Logger logger = LoggerFactory.getLogger(DirectoryStreamCrawler.class);
+    private final Map<String, Exception> failedPaths = new HashMap<>();
 
     @Override
     public void crawlDirectory(Path start, ConcurrentLinkedQueue<Path> queue) throws IOException {
@@ -21,11 +29,17 @@ public class DirectoryStreamCrawler implements Crawler {
                 try (final DirectoryStream<Path> newPaths = Files.newDirectoryStream(p.toAbsolutePath())) {
                     listDirectories(queue, newPaths);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    logger.error("Error when traversing directory {}, with exception {}", p, e);
+                    failedPaths.put(p.toString(), e);
                 }
             } else {
                 queue.add(p);
             }
         });
+    }
+
+    @Override
+    public Map<String, Exception> getFailed() {
+        return Collections.unmodifiableMap(failedPaths);
     }
 }
--- src/main/java/com/atlassian/migration/datacenter/core/fs/Uploader.java
+++ src/main/java/com/atlassian/migration/datacenter/core/fs/Uploader.java
@@ -1,8 +1,10 @@
 package com.atlassian.migration.datacenter.core.fs;
 
+import com.atlassian.migration.datacenter.spi.FailableOperation;
+
 import java.nio.file.Path;
 import java.util.Queue;
 
-public interface Uploader {
+public interface Uploader extends FailableOperation {
     void upload(Queue<Path> queue);
 }
