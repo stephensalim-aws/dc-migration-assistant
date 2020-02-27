461b52f94579947533d3c7e1f2dbf3a012487463
Ben Partridge <bpartridge@atlassian.com>
2020-02-27 15:48:08 +1100

    CHET-86: Implement tests for DefaultFileSystemMigrationProgress

---
+++ src/test/java/com/atlassian/migration/datacenter/core/fs/reporting/DefaultFileSystemMigrationProgressTest.java
@@ -0,0 +1,36 @@
+package com.atlassian.migration.datacenter.core.fs.reporting;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DefaultFileSystemMigrationProgressTest {
+
+    private DefaultFilesystemMigrationProgress sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new DefaultFilesystemMigrationProgress();
+    }
+
+    @Test
+    void shouldBeInitialisedWithNoCompleteFiles() {
+        assertTrue(sut.getMigratedFiles().isEmpty(), "expected migrated files list of new progress to be empty");
+    }
+
+    @Test
+    void shouldAddMigratedFileToMigratedFiles() {
+        final Path testFile = Paths.get("file");
+        sut.reportFileMigrated(testFile);
+
+        assertEquals(1, sut.getMigratedFiles().size());
+
+        final Path migratedFile = sut.getMigratedFiles().get(0);
+        assertEquals(testFile, migratedFile);
+    }
+}
