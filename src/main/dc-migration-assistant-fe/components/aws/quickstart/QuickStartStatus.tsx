1586314780e17fd62ddbe0898846241ada07892d
Varun Arbatti <1063972+theghostwhoforks@users.noreply.github.com>
2020-02-27 13:59:33 +1100

    CHET-79: Add flag to view status of stack on quickstart status component

--- src/main/dc-migration-assistant-fe/components/aws/quickstart/QuickStartStatus.tsx
+++ src/main/dc-migration-assistant-fe/components/aws/quickstart/QuickStartStatus.tsx
@@ -1,33 +1,51 @@
 import React, { FunctionComponent, ReactElement, useEffect, useState } from 'react';
 import styled from 'styled-components';
-import Button, { ButtonAppearances } from '@atlaskit/button';
+import SuccessIcon from '@atlaskit/icon/glyph/check-circle';
+import ErrorIcon from '@atlaskit/icon/glyph/error';
 import Spinner from '@atlaskit/spinner';
-import { BrowserRouterProps } from 'react-router-dom';
+import Flag from '@atlaskit/flag';
 import { callAppRest } from '../../../utils/api';
 import { awsStackStatusRestPath } from '../../../utils/RoutePaths';
 
 const QuickStartStatusContainer = styled.div`
-    margin-top: 250px;
-    align-self: flex-start;
+    display: flex;
 `;
 
-const proceedToFileSystemMigrateButton = (isStackProvisioned: boolean): ReactElement => {
+const stageStatusFlag = (currentProvisioningStatus: string): ReactElement => {
     return (
-        <Button appearance={isStackProvisioned ? 'primary' : 'warning'}>
-            Proceed to Filesystem migration
-        </Button>
+        <Flag
+            actions={[
+                {
+                    content: 'CloudFormation Console',
+                    onClick: () => {},
+                },
+            ]}
+            icon={
+                currentProvisioningStatus === 'CREATE_COMPLETE' ? (
+                    <SuccessIcon primaryColor="#36B37E" label="Success" />
+                ) : currentProvisioningStatus === 'CREATE_IN_PROGRESS' ? (
+                    <SuccessIcon primaryColor="#FFC400" label="InProgress" />
+                ) : (
+                    <ErrorIcon primaryColor="#FF5630" label="Failure" />
+                )
+            }
+            description="All good things take time. Like your next Uber Eats delivery!"
+            id="1"
+            key="1"
+            title="Provisioning Status"
+        />
     );
 };
 
 export const QuickStartStatus: FunctionComponent = (props: Record<any, any>): ReactElement => {
     const [inProgress, setInProgress]: [boolean, Function] = useState(true);
-    const [provisioningFailed, setProvisioningFailed]: [boolean, Function] = useState(false);
+    const [provisioningStatus, setProvisioningStatus]: [string, Function] = useState('UNKNOWN');
 
     const getStackStatusFromApi = (stackIdentifier: string): Promise<any> => {
         return callAppRest('GET', awsStackStatusRestPath.replace(':stackId:', stackIdentifier))
             .then(response => {
                 if (response.status !== 200) {
-                    throw Error('Stack not found');
+                    throw Error('FAILED');
                 }
                 return response;
             })
@@ -35,11 +53,14 @@ export const QuickStartStatus: FunctionComponent = (props: Record<any, any>): Re
             .then(status => {
                 if (status === 'CREATE_COMPLETE') {
                     setInProgress(false);
-                    setProvisioningFailed(false);
+                    setProvisioningStatus(status);
+                } else {
+                    setProvisioningStatus(status);
+                    setInProgress(true);
                 }
             })
             .catch(err => {
-                setProvisioningFailed(true);
+                setProvisioningStatus(err.toString());
                 setInProgress(false);
             });
     };
@@ -52,11 +73,11 @@ export const QuickStartStatus: FunctionComponent = (props: Record<any, any>): Re
                 'Retrieved status from API once. TODO: periodically poll this API endpoint'
             );
         });
-    });
+    }, []);
 
     return (
         <QuickStartStatusContainer>
-            {inProgress ? <Spinner /> : proceedToFileSystemMigrateButton(provisioningFailed)}
+            {inProgress ? <Spinner /> : stageStatusFlag(provisioningStatus)}
         </QuickStartStatusContainer>
     );
 };
