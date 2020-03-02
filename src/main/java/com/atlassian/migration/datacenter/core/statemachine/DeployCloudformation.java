13e384ac4ecd98dfb28e5e0a32dc61d30c8f60da
Ben Partridge <bpartridge@atlassian.com>
2020-03-02 16:49:36 +1100

    CHET-128: Implement logic for deploying stack in DeployCloudformation state

--- src/main/java/com/atlassian/migration/datacenter/core/statemachine/DeployCloudformation.java
+++ src/main/java/com/atlassian/migration/datacenter/core/statemachine/DeployCloudformation.java
@@ -1,11 +1,21 @@
 package com.atlassian.migration.datacenter.core.statemachine;
 
+import com.atlassian.migration.datacenter.core.aws.CfnApi;
 import com.atlassian.migration.datacenter.spi.statemachine.State;
 
 public class DeployCloudformation implements State {
+
+    private final AWSMigrationContext context;
+    private final CfnApi cloudformationApi;
+
+    public DeployCloudformation(AWSMigrationContext context, CfnApi cloudformationApi) {
+        this.context = context;
+        this.cloudformationApi = cloudformationApi;
+    }
+
     @Override
     public void run() {
-
+        cloudformationApi.provisionStack(context.getAppTemplateURL(), context.getAppStackName(), context.getAppTemplateParameters());
     }
 
     @Override
--- src/test/java/com/atlassian/migration/datacenter/core/statemachine/DeployCloudformationTest.java
+++ src/test/java/com/atlassian/migration/datacenter/core/statemachine/DeployCloudformationTest.java
@@ -1,17 +1,34 @@
 package com.atlassian.migration.datacenter.core.statemachine;
 
+import com.atlassian.migration.datacenter.core.aws.CfnApi;
 import com.atlassian.migration.datacenter.spi.statemachine.State;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class DeployCloudformationTest {
+
     private DeployCloudformation sut;
+
+    @Mock
+    CfnApi mockCfnApi;
+
+    @Mock
+    AWSMigrationContext mockMigrationContext;
 
     @BeforeEach
     void setUp() {
-        sut = new DeployCloudformation();
+        sut = new DeployCloudformation(mockMigrationContext, mockCfnApi);
     }
 
     @Test
@@ -19,6 +36,22 @@ class DeployCloudformationTest {
         State nextState = sut.nextState();
 
         assertEquals(WaitCloudformation.class, nextState.getClass());
+    }
+
+    @Test
+    void shouldInitiateDeploymentWhenRun() {
+        final Map<String, String> params = new HashMap<>();
+        params.put("Parameter1", "value");
+        final String stackName = "stack name";
+        final String templateUrl = "s3://bucket/template.yml";
+
+        when(mockMigrationContext.getAppStackName()).thenReturn(stackName);
+        when(mockMigrationContext.getAppTemplateURL()).thenReturn(templateUrl);
+        when(mockMigrationContext.getAppTemplateParameters()).thenReturn(params);
+
+        sut.run();
+
+        verify(mockCfnApi).provisionStack(templateUrl, stackName, params);
     }
 
 }
