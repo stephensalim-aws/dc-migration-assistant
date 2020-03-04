796dae681250659a935f7a75d59174cca6e7443f
Ben Partridge <bpartridge@atlassian.com>
2020-03-04 13:22:46 +1100

    Implement logic for reporting files found for migration in DefaultFilesystemMigrationProgress

--- src/main/java/com/atlassian/migration/datacenter/core/fs/reporting/DefaultFilesystemMigrationProgress.java
+++ src/main/java/com/atlassian/migration/datacenter/core/fs/reporting/DefaultFilesystemMigrationProgress.java
@@ -5,19 +5,22 @@ import com.atlassian.migration.datacenter.spi.fs.reporting.FileSystemMigrationPr
 import java.nio.file.Path;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class DefaultFilesystemMigrationProgress implements FileSystemMigrationProgress {
 
     private List<Path> migratedFiles = new LinkedList<>();
 
+    private AtomicLong filesFound = new AtomicLong(0);
+
     @Override
     public Long getNumberOfFilesFound() {
-        return null;
+        return filesFound.get();
     }
 
     @Override
     public void reportFileFound() {
-
+        filesFound.incrementAndGet();
     }
 
     @Override
