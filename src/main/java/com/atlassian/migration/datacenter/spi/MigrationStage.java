f559c0ebf4917159cee62bf5efd4246042136bff
Ben Partridge <bpartridge@atlassian.com>
2020-03-05 11:17:09 +1100

    CHET-139: Add remaining states and rename up/down migration states to Export/Import so as to encapsulate the "loading" of the imported data (i.e. pg_restore)

--- src/main/java/com/atlassian/migration/datacenter/spi/MigrationStage.java
+++ src/main/java/com/atlassian/migration/datacenter/spi/MigrationStage.java
@@ -9,7 +9,12 @@ public enum MigrationStage {
     AUTHENTICATION("authentication"),
     PROVISION_APPLICATION("provision_app"), WAIT_PROVISION_APPLICATION("wait_provision_app"),
     PROVISION_MIGRATION_STACK("provision_migration"), WAIT_PROVISION_MIGRATION_STACK("wait_provision_migration"),
-    READY_FS_MIGRATION("ready_fs_migration"), FS_MIGRATION_UP("fs_migration_up"), FS_MIGRATION_DOWN("fs_migration_down");
+    READY_FS_MIGRATION("ready_fs_migration"), FS_MIGRATION_EXPORT("fs_migration_up"), FS_MIGRATION_IMPORT("fs_migration_down"),
+    OFFLINE_WARNING("cutover_warning"),
+    DB_MIGRATION_EXPORT("db_migration_up"), DB_MIGRATION_IMPORT("db_migration_down"),
+    VALIDATE("validate"),
+    CUTOVER("cutover"),
+    FINISHED("finished");
 
     private String key;
 
