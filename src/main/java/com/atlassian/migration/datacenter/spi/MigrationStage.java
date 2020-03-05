485fd62a87d98b0942f37526ad8c4d41ac3d5bcb
Ben Partridge <bpartridge@atlassian.com>
2020-03-05 11:07:30 +1100

    CHET-139: Refactor migrations states based upon agreed upon states

--- src/main/java/com/atlassian/migration/datacenter/spi/MigrationStage.java
+++ src/main/java/com/atlassian/migration/datacenter/spi/MigrationStage.java
@@ -4,9 +4,12 @@ package com.atlassian.migration.datacenter.spi;
  * Represents all possible states of an on-premise to cloud migration.
  */
 public enum MigrationStage {
-    NOT_STARTED(""), STARTED("started"),
-    READY_TO_PROVISION("ready_to_provision"), PROVISIONING_IN_PROGRESS("provisioning_in_progress"), PROVISIONING_COMPLETE("provisioning_complete"), PROVISIONING_ERROR("provisioning_error"),
-    READY_FS_MIGRATION("ready_fs_migration"), FS_MIGRATION("fs_migration");
+    NOT_STARTED(""),
+    ERROR("error"),
+    AUTHENTICATION("authentication"),
+    PROVISION_APPLICATION("provision_app"), WAIT_PROVISION_APPLICATION("wait_provision_app"),
+    PROVISION_MIGRATION_STACK("provision_migration"), WAIT_PROVISION_MIGRATION_STACK("wait_provision_migration"),
+    READY_FS_MIGRATION("ready_fs_migration"), FS_MIGRATION_UP("fs_migration_up"), FS_MIGRATION_DOWN("fs_migration_down");
 
     private String key;
 
