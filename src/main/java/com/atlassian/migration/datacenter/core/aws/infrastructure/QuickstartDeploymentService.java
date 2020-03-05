b83aad3283e49faf6ecb62fc24313b57ed67147d
Ben Partridge <bpartridge@atlassian.com>
2020-03-05 15:40:38 +1100

    CHET-128: Implement deployment of quick start in QuickstartDeploymentService

--- src/main/java/com/atlassian/migration/datacenter/core/aws/infrastructure/QuickstartDeploymentService.java
+++ src/main/java/com/atlassian/migration/datacenter/core/aws/infrastructure/QuickstartDeploymentService.java
@@ -1,14 +1,23 @@
 package com.atlassian.migration.datacenter.core.aws.infrastructure;
 
+import com.atlassian.migration.datacenter.core.aws.CfnApi;
 import com.atlassian.migration.datacenter.spi.infrastructure.ApplicationDeploymentService;
 
 import java.util.Map;
 
 public class QuickstartDeploymentService implements ApplicationDeploymentService {
 
+    private static final String QUICKSTART_TEMPLATE_URL = "https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc-with-vpc.template.yaml";
+
+    private CfnApi cfnApi;
+
+    public QuickstartDeploymentService(CfnApi cfnApi) {
+        this.cfnApi = cfnApi;
+    }
+
     @Override
     public void deployApplication(String deploymentId, Map<String, String> params) {
-
+        cfnApi.provisionStack(QUICKSTART_TEMPLATE_URL, deploymentId, params);
     }
 
     @Override
--- src/test/java/com/atlassian/migration/datacenter/core/aws/infrastructure/QuickstartDeploymentServiceTest.java
+++ src/test/java/com/atlassian/migration/datacenter/core/aws/infrastructure/QuickstartDeploymentServiceTest.java
@@ -1,31 +1,34 @@
 package com.atlassian.migration.datacenter.core.aws.infrastructure;
 
 import com.atlassian.migration.datacenter.core.aws.CfnApi;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashMap;
 
 import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class QuickstartDeploymentServiceTest {
-
-    QuickstartDeploymentService deploymentService;
 
     @Mock
     CfnApi mockCfnApi;
 
-    @BeforeEach
-    void setUp() {
-        deploymentService = new QuickstartDeploymentService();
-    }
+    @InjectMocks
+    QuickstartDeploymentService deploymentService;
 
     @Test
     void shouldDeployQuickStart() {
-        deploymentService.deployApplication("my-stack", new HashMap<>());
+        final String stackName = "my-stack";
+        final HashMap<String, String> stackParams = new HashMap<>();
+        stackParams.put("parameter", "value");
 
-        verify(mockCfnApi).provisionStack("https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc-with-vpc.template.yml", "my-stack", new HashMap<>());
+        deploymentService.deployApplication(stackName, stackParams);
+
+        verify(mockCfnApi).provisionStack("https://aws-quickstart.s3.amazonaws.com/quickstart-atlassian-jira/templates/quickstart-jira-dc-with-vpc.template.yaml", stackName, stackParams);
     }
 
 }
