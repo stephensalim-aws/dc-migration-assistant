c9e9f8e45c7c0ae3162ed1db8a09bd32e8fd8c4d
Ben Partridge <bpartridge@atlassian.com>
2020-03-05 15:11:07 +1100

    add documentation to MigrationServiceV2 class

--- src/main/java/com/atlassian/migration/datacenter/spi/MigrationServiceV2.java
+++ src/main/java/com/atlassian/migration/datacenter/spi/MigrationServiceV2.java
@@ -3,14 +3,27 @@ package com.atlassian.migration.datacenter.spi;
 import com.atlassian.migration.datacenter.core.exceptions.InvalidMigrationStageError;
 import com.atlassian.migration.datacenter.dto.Migration;
 
+/**
+ * Manages the lifecycle of the migration
+ */
 public interface MigrationServiceV2 {
 
+    /**
+     * Creates a new migration in the initial stage
+     */
     Migration createMigration();
 
+    /**
+     * Gets the current stage of the migration
+     */
     MigrationStage getCurrentStage();
 
     void transition(MigrationStage from, MigrationStage to) throws InvalidMigrationStageError;
 
+    /**
+     * Moves the migration into an error stage
+     * @see MigrationStage#ERROR
+     */
     void error();
 
 }
