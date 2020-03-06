ad373e4b01f12ac0e75032b149dce90f5477d806
Ben Partridge <bpartridge@atlassian.com>
2020-03-06 14:33:02 +1100

    CHET-131: Implement cloud configuration service to manage Authentication and region configuratin

---
+++ src/main/java/com/atlassian/migration/datacenter/spi/cloud/CloudConfigurationService.java
@@ -0,0 +1,13 @@
+package com.atlassian.migration.datacenter.spi.cloud;
+
+public interface CloudConfigurationService {
+
+    /**
+     * Configures access to a cloud provider to facilitate the migration
+     * @param entity an identifier for the entity accessing the cloud provider e.g. IAM user
+     * @param secret the secret to authenticate the entity to the cloud provider e.g. secret access key
+     * @param geography the geography to use for deployment of resources e.g. AWS region
+     */
+    void configureCloudProvider(String entity, String secret, String geography);
+
+}
