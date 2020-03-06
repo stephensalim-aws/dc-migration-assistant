c3eeabb0b4a0291cecdc9aa05ca02d14309b9196
Varun <1063972+theghostwhoforks@users.noreply.github.com>
2020-03-06 14:45:50 +1100

    Add an integration test to test Cfn API with localstack. (#42)
    * Add an integration test to test Cfn API with localstack.
    
    * Use StubAwsCredentials for integration tests. Update docker localstack version to 0.10.8

--- src/test/java/com/atlassian/migration/datacenter/core/aws/CfnApiIT.java
+++ src/test/java/com/atlassian/migration/datacenter/core/aws/CfnApiIT.java
@@ -1,18 +1,32 @@
 package com.atlassian.migration.datacenter.core.aws;
 
-import com.atlassian.migration.datacenter.core.aws.auth.AtlassianPluginAWSCredentialsProvider;
-import com.atlassian.migration.datacenter.core.aws.region.InvalidAWSRegionException;
-import com.atlassian.migration.datacenter.core.aws.region.PluginSettingsRegionManager;
-import com.atlassian.migration.datacenter.core.aws.region.RegionService;
+import cloud.localstack.docker.LocalstackDockerExtension;
+import cloud.localstack.docker.annotation.LocalstackDockerProperties;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import org.junit.jupiter.api.extension.ExtendWith;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudformation.CloudFormationAsyncClient;
 import software.amazon.awssdk.services.cloudformation.model.Stack;
 import software.amazon.awssdk.services.cloudformation.model.StackInstanceNotFoundException;
 import software.amazon.awssdk.services.cloudformation.model.StackStatus;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Objects;
 import java.util.Optional;
@@ -26,38 +40,55 @@ import java.util.function.Function;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-
-/**
- * Test Integration test. No mocks involved
- */
+@Tag("integration")
+@ExtendWith({LocalstackDockerExtension.class})
+@LocalstackDockerProperties(services = {"cloudformation", "s3"}, imageTag = "0.10.8")
 class CfnApiIT {
 
-    public static final String S3_CFN_STACK_URL = "https://dcd-slinghost-templates.s3-ap-southeast-2.amazonaws.com/tests/create_s3_bucket.yaml";
     private CfnApi cfnApi;
+
+    static final URI LOCALSTACK_CLOUDFORMATION_URI = URI.create("http://localhost:4581");
+    static final URI LOCALSTACK_S3_URI = URI.create("http://localhost:4572");
+    static final String CFN_TEMPLATE_S3_BUCKET_CREATE_JSON = "/cfn/create_s3_bucket.json";
+
+    static final String S3_BUCKET_NAME = "dcd-slingshot-templates";
+    static final String S3_TEMPLATE_BUCKET_KEY = "create_s3_bucket.json";
+    static final String S3_CFN_STACK_URL = String.format("%s/%s/%s", LOCALSTACK_S3_URI.toString(), S3_BUCKET_NAME, S3_TEMPLATE_BUCKET_KEY);
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        S3Client s3Client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .endpointOverride(LOCALSTACK_S3_URI)
+                .credentialsProvider(new StubAwsCredentialsProvider())
+                .build();
+
+        s3Client.createBucket(CreateBucketRequest.builder().bucket(S3_BUCKET_NAME).build());
+
+        InputStream inputStream = CfnApiIT.class.getResourceAsStream(CFN_TEMPLATE_S3_BUCKET_CREATE_JSON);
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(inputStream, writer);
+
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder().bucket(S3_BUCKET_NAME).key(S3_TEMPLATE_BUCKET_KEY).acl(ObjectCannedACL.PUBLIC_READ).build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromString(writer.toString()));
+
+        ResponseInputStream<GetObjectResponse> object = s3Client.getObject(GetObjectRequest.builder().bucket(S3_BUCKET_NAME).key(S3_TEMPLATE_BUCKET_KEY).build());
+
+        assertEquals(object.response().contentLength(), writer.toString().length());
+    }
 
     @BeforeEach
     void setUp() {
-        RegionService regionManager = new RegionService() {
-            @Override
-            public String getRegion() { return System.getenv("AWS_DEFAULT_REGION"); }
-
-            @Override
-            public void storeRegion(String string) { throw new UnsupportedOperationException(); }
-        };
-        cfnApi = new CfnApi(DefaultCredentialsProvider.create(), regionManager);
+        CloudFormationAsyncClient client = CloudFormationAsyncClient.builder()
+                .credentialsProvider(new StubAwsCredentialsProvider())
+                .region(Region.AP_SOUTHEAST_2)
+                .endpointOverride(LOCALSTACK_CLOUDFORMATION_URI)
+                .build();
+        cfnApi = new CfnApi(client);
     }
 
     @Test
-    @Disabled("Disabled until LocalStack is Created")
-    public void shouldGetStatusOfExistingCfnStack() {
-        String stackArn = "arn:aws:cloudformation:ap-southeast-2:887764444972:stack/jira-analytics-stack-713-001/842750e0-4bbe-11ea-bada-0615bd2a67d8";
-        StackStatus status = cfnApi.getStatus(stackArn);
-        assertEquals(StackStatus.CREATE_COMPLETE, status);
-    }
-
-
-    @Test
-    @Disabled("Disabled until LocalStack is Created")
     public void shouldRaiseExceptionWhenStackDoesNotExist() {
         String stackArn = "arn:aws:cloudformation:ap-southeast-2:1231231231:stack/i-do-not-exist";
         Assertions.assertThrows(StackInstanceNotFoundException.class, () -> {
@@ -66,39 +97,32 @@ class CfnApiIT {
     }
 
     @Test
-    @Disabled("Disabled until LocalStack is Created")
-    public void shouldGetExistingCfnStack() {
-        String stackArn = "arn:aws:cloudformation:ap-southeast-2:887764444972:stack/jira-analytics-stack-713-001/842750e0-4bbe-11ea-bada-0615bd2a67d8";
-        Optional<Stack> stack = cfnApi.getStack(stackArn);
-        assertEquals(StackStatus.CREATE_COMPLETE, stack.get().stackStatus());
-    }
-
-    @Test
-    @Disabled("Disabled until LocalStack is Created")
-        //TODO: Delete stack when complete after each test
     void shouldProvisionNewCfnStack() {
         String random = UUID
                 .randomUUID().toString().split("-")[0];
-        String stackName = String.format("mothra-test-%s", random);
+        String stackName = String.format("trebuchet-test-%s", random);
         Optional<String> provisionedStackId = cfnApi.provisionStack(S3_CFN_STACK_URL, stackName, new HashMap<String, String>() {{
             put("S3BucketName", stackName + "-bucket");
         }});
 
-        assertNotNull(provisionedStackId.get());
+        String stackId = provisionedStackId.get();
+        assertNotNull(stackId);
 
         try {
-            awaitStackCreation(provisionedStackId.get(), cfnApi::getStatus).get(60, TimeUnit.SECONDS);
+            awaitStackCreation(stackName, cfnApi::getStack).get(60, TimeUnit.SECONDS);
         } catch (Exception e) {
             Assertions.fail("Timeout while waiting for stack creation to complete", e);
         }
     }
 
-    private CompletableFuture<String> awaitStackCreation(String stackId, Function<String, StackStatus> statusFunc) {
+    private CompletableFuture<String> awaitStackCreation(String stackId, Function<String, Optional<Stack>> statusFunc) {
         CompletableFuture<String> completableFuture = new CompletableFuture<>();
         ScheduledFuture<?> scheduledFuture = Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
-            StackStatus status = statusFunc.apply(stackId);
-            if (Objects.equals(status, StackStatus.CREATE_COMPLETE)) {
-                completableFuture.complete(stackId);
+            Optional<Stack> stack = statusFunc.apply(stackId);
+            if (stack.isPresent()) {
+                if (Objects.equals(stack.get().stackStatus(), StackStatus.CREATE_COMPLETE)) {
+                    completableFuture.complete(stackId);
+                }
             }
         }, 0, 10, TimeUnit.SECONDS);
 
