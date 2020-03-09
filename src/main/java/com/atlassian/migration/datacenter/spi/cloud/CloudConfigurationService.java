bf1ea324cd6b39b993646f8257c2a4504d677641
Ben Partridge <bpartridge@atlassian.com>
2020-03-09 12:31:11 +1100

    CHET-131: Change javadoc of entity param in CloudConfigurationService#configureCloudProvider to have more accurate examples

--- src/main/java/com/atlassian/migration/datacenter/spi/cloud/CloudConfigurationService.java
+++ src/main/java/com/atlassian/migration/datacenter/spi/cloud/CloudConfigurationService.java
@@ -6,8 +6,8 @@ public interface CloudConfigurationService {
 
     /**
      * Configures access to a cloud provider to facilitate the migration
-     * @param entity an identifier for the entity accessing the cloud provider e.g. IAM user
-     * @param secret the secret to authenticate the entity to the cloud provider e.g. secret access key
+     * @param entity an identifier for the entity accessing the cloud provider e.g. AWS access key ID
+     * @param secret the secret to authenticate the entity to the cloud provider e.g. AWS secret access key
      * @param geography the geography to use for deployment of resources e.g. AWS region
      */
     void configureCloudProvider(String entity, String secret, String geography) throws InvalidMigrationStageError;
