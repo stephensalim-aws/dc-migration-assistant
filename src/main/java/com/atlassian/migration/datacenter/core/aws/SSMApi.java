28e02234a4a8aa3c494f84df0f91dcf7b4356d6f
Ben Partridge <bpartridge@atlassian.com>
2020-03-10 15:03:51 +1100

    CHET-154: implement and test running SSM commands

--- src/main/java/com/atlassian/migration/datacenter/core/aws/SSMApi.java
+++ src/main/java/com/atlassian/migration/datacenter/core/aws/SSMApi.java
@@ -4,18 +4,47 @@ import com.atlassian.migration.datacenter.core.aws.region.RegionService;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.SendCommandRequest;
+import software.amazon.awssdk.services.ssm.model.SendCommandResponse;
+
+import java.util.List;
+import java.util.Map;
 
 public class SSMApi {
 
     private final AwsCredentialsProvider credentialsService;
     private final RegionService regionService;
 
-    // This may be null, use getClient to safely get a non-null SSM client
+    // This may be null, use getClient() to safely get a non-null SSM client
     private SsmClient client;
 
     public SSMApi(AwsCredentialsProvider credentialsService, RegionService regionService) {
         this.credentialsService = credentialsService;
         this.regionService = regionService;
+    }
+
+    /**
+     * Runs an SSM automatiom document against a specific EC2 instance with the specified parameters.
+     * @param documentName The name of the document to run. The latest version will be used.
+     * @param targetEc2InstanceId The instance ID of the EC2 instance to run this command on
+     * @param commandParameters
+     * @return the command ID of the invoked command.
+     */
+    public String runSSMDocument(String documentName, String targetEc2InstanceId, Map<String, List<String>> commandParameters) {
+        SendCommandRequest request = SendCommandRequest.builder()
+                .documentName(documentName)
+                .documentVersion("$LATEST")
+                .instanceIds(targetEc2InstanceId)
+                .parameters(commandParameters)
+                .timeoutSeconds(600)
+                .comment("command run by Jira DC Migration Assistant")
+                .outputS3BucketName("migration-bucket")
+                .outputS3KeyPrefix("fs-copy-down-log")
+                .build();
+
+        SendCommandResponse response = getClient().sendCommand(request);
+
+        return response.command().commandId();
     }
 
     // Lazily instantiates the ssm client
--- src/test/java/com/atlassian/migration/datacenter/core/aws/SSMApiIT.java
+++ src/test/java/com/atlassian/migration/datacenter/core/aws/SSMApiIT.java
@@ -3,11 +3,15 @@ package com.atlassian.migration.datacenter.core.aws;
 import cloud.localstack.TestUtils;
 import cloud.localstack.docker.LocalstackDockerExtension;
 import cloud.localstack.docker.annotation.LocalstackDockerProperties;
+import com.atlassian.migration.datacenter.core.aws.region.InvalidAWSRegionException;
+import com.atlassian.migration.datacenter.core.aws.region.RegionService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetCommandInvocationRequest;
@@ -15,14 +19,50 @@ import software.amazon.awssdk.services.ssm.model.GetCommandInvocationResponse;
 import software.amazon.awssdk.services.ssm.model.SendCommandRequest;
 
 import java.net.URI;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Tag("integration")
-@ExtendWith({LocalstackDockerExtension.class})
-@LocalstackDockerProperties(services = {"cloudformation", "s3", "ssm"}, imageTag = "0.10.8")
+//@ExtendWith({LocalstackDockerExtension.class})
+//@LocalstackDockerProperties(services = {"cloudformation", "s3", "ssm"}, imageTag = "0.10.8")
 public class SSMApiIT {
 
     private static final String LOCALSTACK_SSM_ENDPOINT = "http://localhost:4583";
+
+    private SSMApi sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new SSMApi(DefaultCredentialsProvider.create(), new RegionService() {
+            @Override
+            public String getRegion() {
+                return "us-east-1";
+            }
+
+            @Override
+            public void storeRegion(String string) throws InvalidAWSRegionException {
+
+            }
+        });
+    }
+
+    @Test
+    @Disabled("Requires true authentication to AWS and a specific EC2 instance to be running")
+    void shouldSendSsmCommandToAWS() {
+        final String documentName = "AWS-RunShellScript";
+        // Node ID of ConfluenceGiveMeAllTheData Confluence Node in us-east-1
+        final String targetEc2InstanceId = "i-0d536acd983bade05";
+        final HashMap<String, List<String>> commandParameters = new HashMap<String, List<String>>() {{
+           put("commands", Collections.singletonList("echo 'hello, world'"));
+        }};
+
+        String commandId = sut.runSSMDocument(documentName, targetEc2InstanceId, commandParameters);
+
+        assertNotNull(commandId);
+    }
 
     @Test
     @Disabled("Localstack is giving 500s for SSM send-command")
